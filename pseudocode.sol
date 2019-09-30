e7f7f3f232a9373284cdb6032e33e1a087a49986
Peter Lu <chippermonky@gmail.com>
2019-09-29 17:38:31 -0700

    meow

--- pseudocode.sol
+++ pseudocode.sol
@@ -30,7 +30,10 @@ struct LandStats {
 struct Land {
     string name;
     uint256 value;
+
+    // fixed size array
     Job[] jobs;
+    uint256[] workingAnimals;
 
     LandStats stats;
 
@@ -77,122 +80,278 @@ struct Animal {
     uint64 lastTaxUpdate;
 }
 
-struct PublicPolicy {
-    // tax is over tax/10000 per 1000 blocks
-    uint16 landTax;
-    uint16 animalTax;
+// TODO public policy proposal + voting to pass
 
-    // masters should have at least taxReserve/100 * <taxation per 1000 blocks>
+uint TAX_DENOM = 10000000;
+struct PublicPolicy {
+    // tax is over tax/TAX_DENOM * <value> per block
+    uint32 landTax;
+    uint32 animalTax;
+
+    // masters should have at least taxReserve/100000 * <taxation per block>
     uint16 taxReserve;
+
+
+    // other parameters
+    // severance?
+    // insurance for failure to pay salary or
+    // finders fee per block
+    // finders fee bonus per overdue block
+    // bankrupcy finders fee?
+    // bankrupcy thresheld? (amonut owed > threshold*asset value)
 
 }
 
-library Masters {
-    // TODO use more efficient dense storage since # of times are finite
-    mapping (address=>mapping (ItemType => uint)) items;
-    mapping (address=>uint256) tokens;
-    function addItem(address owner, ItemType itemType, uint quantity) {
-        // TODO do overflow checks on item quantity
-        items[owner][itemType] += quantity;
-    }
+struct Master {
+    // todo use more efficient dense storage since # of times are finite
+    mapping (ItemType => uint) items;
+    uint256 tokens;
+    uint256 publicDebt;
 
+    // TODO total assets value (may cover tax reserve?)
     // TODO total harberger tax reserve liability (to ensure enough funds to pay all tax)
 }
 
+library Masters {
+    mapping (address=>master) masters;
+    function addItem(address owner, ItemType itemType, uint quantity) {
+        // todo do overflow checks on item quantity
+        masters[owner].items[itemType] += quantity;
+    }
+    function balance(address owner) {
+        return tokens[owner];
+    }
+    function subBalance(address owner, uint256 value) {
+        require(value > masters[owner].tokens);
+        masters[owner].tokens -= value;
+    }
+    function addBalance(address owner, uint256 value) {
+        Master storage moster = masters[owner];
+        // pay off debt first
+        if (master.debt > 0 ){
+            // TODO finish
+            //masters[GOV_ADDRESS].addBalance(min(value,master.debt))
+        }
+        masters[owner].tokens += value;
+    }
+    // do we really need this? I don't think we do if we have public debt
+    function checkReserve(address owner, uint32 tax, uint256 amount) returns bool {
+        // TODO check if reserve funds are sufficient to cover increase in tax liability
+        //taxLiability = tax/TAX_DENOM * amount
+    }
+}
+
 // TODO more items
-enum ItemType { CORN, STUFF }
+enum ItemType { NIL_ITEM = 0, CORN, STUFF }
 
 // contract vars
 Masters masters;
 animals mapping(uint256 => Animal);
+lands mapping(uint256 => Land);
+
+PublicPolicy law;
+
+// public owned tokens are stored here
+address GOV_ADDRESS = 0xBABB0;
 
 
+// master functions
+// this taxes the master at tax/TAX_DENOM*amount per block
+// returns amount of taxes that could not be paid due to insufficient balance
+function levyTax(address master, uint16 tax, uint256 amount, uint blocks) returns uint256 {
+    // there is some round off error, prob not a big deal
+    taxPerBlock = tax/TAX_DENOM * amount;
+    taxAmount =  taxPerBlock * blocks;
+    old = masters.balance(master);
+    if old < taxAmount {
+        masters.subBalance(master, taxAmount);
+        return taxAmount-old;
+    }
+    masters.setBalance(master, old-taxAmount);
+    return 0;
+}
+
+function accrueDebt(address owner, uint256 amountOwed) {
+    // TODO
+    // IDK I guess add some sort of public debt?
+    // if public debt exceeds total value of all assets owed, then player really goes bankrupt and relinquishes all assets to the government for auction.
+
+    Master storage master = masters[owner];
+    master.debt += amountOwed;
+
+    // TODO handle force bankrupcy
+    //if(master.debt > master.totalAssetValue*law.bankruptThreshold)
+
+}
 
 // animal transactions
-function SetPrice(uint256 animalId, uint256 price) external {
+function SetAnimalPrice(uint256 animalId, uint256 price) external {
     animal = animals[animalId];
 
+    // ?? do we really need this? I don't think we do if we have public debt
     if(price > animal.price) {
-        // TODO check master's tax reserve liability
+        if(!masters.checkReserve(animal.master, law.animalTax, price - animal.price)) {
+            return;
+        }
     }
 
     // must tax before setting price to ensure lazyness is correct
     taxAnimal(animal);
 
-    animal.price = price;
-
-    // TODO update master tax reserve liability
+    setAnimalPrice(animal, price)
 }
 
-function UpdateAnimal(uint256 animalId) {
-    animal = animals[animalId];
-
-    // TODO finish
-
-    // do we need this?
-    taxAnimal(animal);
-
-    // update hunger
-
-    // update work
-
-    // ?is it a problem if there are future updates in the same block#?
-    // ?maybe 1 action/user limitation (also means you can keep track of oldest animal more easily)
-    animal.lastUpdate = block.number;
-}
 
 function PurchaseAnimal(uint256 animalId, uint256 price) {
     animal = animals[animalId];
 
     // check for sufficient funds
-    require(masters.tokens[msg.sender] > price);
-    masters.tokens[msg.sender] -= price;
+    require(masters.balance(msg.sender) > price);
+    masters.subBalance(msg.sender, price);
+    masters.addBalance(animal.master, price);
 
-    // TODO check master's tax reserve liability
-
-
+    // ?? do we really need this? I don't think we do if we have public debt
+    // check tax reserves are sufficient
+    if(!masters.checkReserve(msg.sender, law.animalTax, price)) {
+        return;
+    }
 
     // update animal so that previous owner gets earnings up until the purchase
     UpdateAnimal(animalId);
 
     // update ownership
-    animal.master = msg.sender
+    animal.master = msg.sender;
 
-    // TODO set the price (same as SetPrice, but remove tax liability check and taxAnimal)
+    setAnimalPrice(animal, price);
 }
+
+function ApplyJob(uint256 animalId, uint256 landId, uint8 jobIndex) {
+    animal = animals[animalId];
+    land = lands[landId];
+
+    // require job to be free
+    require(land.jobs[jobIndex] == 0);
+
+    updateAnimal(animal);
+
+    // first quit job if there is one
+    if(animal.job != 0) {
+        quitJob(animal);
+    }
+
+    // start worknig at the new job
+    startJob(animalId, animal, landId, land, jobIndex);
+}
+
+function updateAnimal(Animal storage animal) {
+
+    // early exit if nothing will happen
+    if(block.number == animal.lastUpdate) {
+        return;
+    }
+
+    // ?? do we need this?
+    taxAnimal(animal);
+
+    // TODO update hunger
+
+    // update work
+    if(animal.job != 0) {
+        work(animal, lands[animal.job.landId], animal.job.jobIndex);
+    }
+
+    // ?? is it a problem if there are future updates in the same block#?
+    // ?? maybe 1 action/user limitation (also means you can keep track of oldest animal more easily)
+    animal.lastUpdate = block.number;
+}
+
 
 // animal internal functions
 function taxAnimal(Animal storage animal) {
-    // TODO charge tax
-
+    owed = levyTax(animal.master, law.animalTax, animal.value, block.number-animal.lastTaxUpdate);
+    if(owed > 0) {
+        accrueDebt(animal.master, owed);
+    }
     animal.lastTaxUpdate = block.number;
 }
 
+function setAnimalPrice(Animal storage animal, uint256 price) {
+    animal.price = price;
+    // TODO update master tax reserve liability
+}
+
+function animalIsUpdated(Animal storage animal) returns bool {
+    return animal.lastUpdate == block.number;
+}
 
 //function SetPrice(uint256 landId, uint256 price) {
 
 // land transactions
-function UpdateLand(Land storage land) {
+function UpdateLand(uint256 landId) {
     // TODO finish
+
+    land = lands[landId];
 
     taxLand(land);
 
-    // TODO force update jobs?
+    // ?? TODO do we update all jobs or no
 
-    // TODO update ecology (or does this happen when jobs get updated?)
+    // ?? TODO update ecology (or does this happen when jobs get updated?)
 
     land.lastUpdate = block.number;
+}
+
+function removeJob(Land storage land, uint jobIndex) {
+    job = land.jobs[jobIndex]
+    if (job != 0) {
+        animal = animals[land.workingAnimals[jobIndex]]
+
+        // boot the animal
+        land.workingAnimals[jobIndex] = 0;
+        animal.job = 0;
+
+        // TODO severance pay
+    }
+    land.jobs[jobIndex] = 0;
+}
+function createJob(Land storage land, uint jobIndex, Job jobDesc) {
+    removeJob(land, jobIndex);
+    land.jobs[jobIndex] = jobDesc;
 }
 
 // land internal functions
 //function taxLand(Land storage land) {
 
 
+
+// animal<->land functions
+function quitJob(Animal storage animal) {
+    require(animalIsUpdated(animal));
+    require(animal.job != 0);
+
+    lands[animal.job.landId].workingAnimals[animal.job.jobIndex] = 0;
+    animal.job = 0;
+}
+
+function startJob(uint256 animalId, Animal storage animal, uint256 landId, Land land, uint8 jobIndex) {
+    require(animalIsUpdated(animal));
+    require(land.workingAnimals[jobIndex] == 0);
+
+    land.workingAnimals[jobIndex] = animalId;
+    animal.job = Job{landId, jobIndex};
+}
+
 function work(Animal storage animal, Land land, Job job)
 {
+    // TODO check that animal has enough energy to work
+        // do something if animal can't work
+
     output = typeProductivity(job) * jobCompat(animal, job) * landCompat(land, job);
     masters.addItem(animal.owner, job.ItemType, output)
+
+    // TODO modify animal stats
 }
+
 
 // lookup/compatability functions
 function jobProductivity(Job job) pure returns float {
