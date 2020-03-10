4debcc1b72ffd890440940cc81cc12d05498d182
pddla <chippermonky@gmail.com>
2020-03-09 21:13:24 -0700

    4 to 2 spaces

--- potato_game_code/contracts/potato/PotatoERC20.sol
+++ potato_game_code/contracts/potato/PotatoERC20.sol
@@ -9,75 +9,75 @@ import "./Government.sol";
  * ERC20 for resources
  */
 contract PotatoERC20 is ERC20 {
-    using SafeMath for uint256;
+  using SafeMath for uint256;
 
-    event GovTransfer(address from, address to, uint256 amount);
+  event GovTransfer(address from, address to, uint256 amount);
 
-    Government government;
+  Government government;
 
-    constructor(address _gov) public {
-        government = Government(_gov);
-    }
+  constructor(address _gov) public {
+    government = Government(_gov);
+  }
 
-    /**
-     * modified `transfer` that checks if law allows corporation to directly transfer tokens
-     *
-     * Requirements:
-     * - **government authorizes outside resource transfer**
-     * - `recipient` cannot be the zero address.
-     * - the caller must have a balance of at least `amount`.
-     */
-    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
-        require(government.allowOutsideResourceTransfer(address(this)));
-        return super.transfer(recipient, amount);
-    }
+  /**
+   * modified `transfer` that checks if law allows corporation to directly transfer tokens
+   *
+   * Requirements:
+   * - **government authorizes outside resource transfer**
+   * - `recipient` cannot be the zero address.
+   * - the caller must have a balance of at least `amount`.
+   */
+  function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+    require(government.allowOutsideResourceTransfer(address(this)));
+    return super.transfer(recipient, amount);
+  }
 
-    /**
-     * modified `transferFrom` that checks if law allows corporation to directly transfer tokens
-     *
-     * Emits an {Approval} event indicating the updated allowance. This is not
-     * required by the EIP. See the note at the beginning of {ERC20};
-     *
-     * Requirements:
-     * - **government authorizes outside resource transfer**
-     * - `sender` and `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     * - the caller must have allowance for `sender`'s tokens of at least
-     * `amount`.
-     */
-    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
-        require(government.allowOutsideResourceTransfer(address(this)));
-        return super.transferFrom(sender, recipient, amount);
-    }
+  /**
+   * modified `transferFrom` that checks if law allows corporation to directly transfer tokens
+   *
+   * Emits an {Approval} event indicating the updated allowance. This is not
+   * required by the EIP. See the note at the beginning of {ERC20};
+   *
+   * Requirements:
+   * - **government authorizes outside resource transfer**
+   * - `sender` and `recipient` cannot be the zero address.
+   * - `sender` must have a balance of at least `amount`.
+   * - the caller must have allowance for `sender`'s tokens of at least
+   * `amount`.
+   */
+  function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
+    require(government.allowOutsideResourceTransfer(address(this)));
+    return super.transferFrom(sender, recipient, amount);
+  }
 
-    /**
-     * transfer operation that only allows government recognized contracts to transfer tokens and allows them to transfer any amount
-     *
-     * Requirements:
-     * - **the caller must be authorized by the government.**
-     * - `sender` and `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     */
-    function govTransferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
-        require(government.isAuthorizedForTransfer(address(this), _msgSender()));
-        _transfer(sender, recipient, amount);
-        emit GovTransfer(sender, recipient, amount);
-        return true;
-    }
+  /**
+   * transfer operation that only allows government recognized contracts to transfer tokens and allows them to transfer any amount
+   *
+   * Requirements:
+   * - **the caller must be authorized by the government.**
+   * - `sender` and `recipient` cannot be the zero address.
+   * - `sender` must have a balance of at least `amount`.
+   */
+  function govTransferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+    require(government.isAuthorizedForTransfer(address(this), _msgSender()));
+    _transfer(sender, recipient, amount);
+    emit GovTransfer(sender, recipient, amount);
+    return true;
+  }
 
-    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
-     * the total supply. Note that the government does not actually mint tokens in most cases.
-     * Usually, tokens are created through labor.
-     *
-     * Emits a {Transfer} event with `from` set to the zero address.
-     *
-     * Requirements
-     *
-     * - can only be called by authorized contract
-     */
-    function govMint(address account, uint256 amount) public {
-        require(address(government) == _msgSender());
-        _mint(account, amount);
-    }
+  /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+   * the total supply. Note that the government does not actually mint tokens in most cases.
+   * Usually, tokens are created through labor.
+   *
+   * Emits a {Transfer} event with `from` set to the zero address.
+   *
+   * Requirements
+   *
+   * - can only be called by authorized contract
+   */
+  function govMint(address account, uint256 amount) public {
+    require(address(government) == _msgSender());
+    _mint(account, amount);
+  }
 
 }
--- potato_game_code/contracts/potato/PotatoERC721.sol
+++ potato_game_code/contracts/potato/PotatoERC721.sol
@@ -9,26 +9,26 @@ import "./Government.sol";
  * ERC721 for assets
  */
 contract PotatoERC721 is ERC721 {
-    using SafeMath for uint256;
+  using SafeMath for uint256;
 
 
-    Government government;
+  Government government;
 
-    constructor(address _gov) public {
-        government = Government(_gov);
-    }
+  constructor(address _gov) public {
+    government = Government(_gov);
+  }
 
-    // TODO handle taxes before outside transfer
-    // modified `transferFrom` that checks if law allows corporation to directly transfer tokens
-    function transferFrom(address from, address to, uint256 tokenId) public override {
-        require(government.allowOutsideAssetTransfer(address(this)));
-        return super.transferFrom(from, to, tokenId);
-    }
+  // TODO handle taxes before outside transfer
+  // modified `transferFrom` that checks if law allows corporation to directly transfer tokens
+  function transferFrom(address from, address to, uint256 tokenId) public override {
+    require(government.allowOutsideAssetTransfer(address(this)));
+    return super.transferFrom(from, to, tokenId);
+  }
 
-    // TODO handle taxes before outside transfer
-    // modified `safeTransferFrom` that checks if law allows corporation to directly transfer tokens
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public override {
-        require(government.allowOutsideAssetTransfer(address(this)));
-        _safeTransferFrom(from, to, tokenId, _data);
-    }
+  // TODO handle taxes before outside transfer
+  // modified `safeTransferFrom` that checks if law allows corporation to directly transfer tokens
+  function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public override {
+    require(government.allowOutsideAssetTransfer(address(this)));
+    _safeTransferFrom(from, to, tokenId, _data);
+  }
 }
