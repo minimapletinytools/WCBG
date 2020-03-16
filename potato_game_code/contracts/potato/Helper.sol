3557f03e6239a185a78b4563dc32d7f55146c8b5
pddla <chippermonky@gmail.com>
2020-03-15 19:39:29 -0700

    cleanup to make repo more presentable

--- potato_game_code/contracts/potato/Helper.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.6.0;
-
-contract Helper {
-  function codeSize(address _addr) public view returns (uint256 o_code) {
-    assembly {
-      o_code := extcodesize(_addr)
-    }
-  }
-}
